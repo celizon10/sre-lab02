81ea19737f051840a74d74b861eca92226c7369a
celizon10 <isaacelizon@gmail.com>
2024-04-03 17:57:05 -0600

    update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'awslabproject.cj4geok6kpbh.us-east-2.rds.amazonaws.com:3306'
 mysql.init_app(app)
 
-app.secret_key = 'app secret key'
+# app.secret_key = 'app secret key'
 
 
 
